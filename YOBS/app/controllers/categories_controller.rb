98a90799aa36eb7c12cd57352ac236d72dcd3fcd
David Wilbanks <dwilbank@gmail.com>
2014-05-02 08:58:28 -0700

    friday morning

---
+++ YOBS/app/controllers/categories_controller.rb
@@ -0,0 +1,12 @@
+class CategoriesController < ApplicationController
+  def index
+  	@categories = Category.all
+  end
+
+  def show
+  	puts "*"*30
+  	p params
+	puts "*"*30
+  	@category = Category.find(params[:id])
+  end
+end
