55f9986b648d5a09d2abe64672f77f96610c2709
Hembaba & laura <pair+hembaba.llcossette@devbootcamp.com>
2014-05-02 12:20:08 -0700

    * adds 2 category controller tests for index route * adds factory for category in factories.rb

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe CategoriesController do
+  context "#index" do
+    it "is succesful" do
+      get :index
+      expect(response).to be_success
+    end
+
+    it "should assign @categories to Categories.all" do
+      get :index
+      expect(assigns(:categories)).to eq Categories.all?
+    end
+  end
+
+end
