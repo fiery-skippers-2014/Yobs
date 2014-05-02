651436b209b50c4d42f00b99db69e01e09be6dc9
Hembaba & laura <pair+hembaba.llcossette@devbootcamp.com>
2014-05-02 10:53:06 -0700

    * adds routes for categories * adds categories controller * adds category index view

--- YOBS/config/routes.rb
+++ YOBS/config/routes.rb
@@ -1,4 +1,5 @@
 YOBS::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -11,7 +12,7 @@ YOBS::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :categories
 
   # Sample resource route with options:
   #   resources :products do
@@ -48,7 +49,7 @@ YOBS::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'categories#index'
 
   # See how all your routes lay out with "rake routes"
 
