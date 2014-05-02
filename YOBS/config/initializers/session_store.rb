806a1a3ce1dd8efd52767816c877608f6c8a47e0
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-02 10:57:37 -0700

    Fixed the directory structure

--- YOBS/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-YOBS::Application.config.session_store :cookie_store, key: '_YOBS_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# YOBS::Application.config.session_store :active_record_store
