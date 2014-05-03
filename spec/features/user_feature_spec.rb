52fad83c45990be39d388e81a393be281ce29e22
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-03 15:00:15 -0700

    Completed User Feature Tests   * added Logout Test   * used sleep to get test to work (Will refactor)
    TODO: Get the roles working to redirect to correct page

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -1,29 +1,42 @@
 require 'spec_helper'
 
-
  describe 'Guest users' do
   context 'can create an account on the site' do
     let(:user) {FactoryGirl.create :user}
     it "can fill out the Create a User form and create an account" do
-        visit root_path
-        fill_in 'user[user_name]', :with => user.user_name
-        fill_in 'user[first_name]', :with => user.first_name
-        fill_in 'user[last_name]', :with => user.last_name
-        fill_in 'user[email]', :with => user.email
-        fill_in 'user[password]', :with => user.password
-        click_on "Create User"
-        expect(page).to have_content user.user_name
+      visit root_path
+      fill_in 'user[user_name]', :with => user.user_name
+      fill_in 'user[first_name]', :with => user.first_name
+      fill_in 'user[last_name]', :with => user.last_name
+      fill_in 'user[email]', :with => user.email
+      fill_in 'user[password]', :with => user.password
+      click_on "Create User"
+      expect(page).to have_content user.user_name
     end
   end
 
   context 'can login with valid email and password' do
     let!(:user) {FactoryGirl.create :user}
-    it "can fill out the Create a User form and create an account" do
-        visit root_path
-        fill_in 'email', :with => user.email
-        fill_in 'password', :with => user.password
-        click_on "Log in"
-        expect(page).to have_content user.user_name
+    it "can sign in" do
+      visit root_path
+      fill_in 'email', :with => user.email
+      fill_in 'password', :with => user.password
+      click_on "Log in"
+      expect(page).to have_content user.user_name
+    end
+  end
+
+
+  context 'can log out' do
+    let(:user) {FactoryGirl.create :user}
+    before(:each) do
+      ApplicationController.any_instance.stub(:current_user).and_return(user)
+    end
+    it "can log out" do
+      visit root_path
+      click_on "Log Out"
+      sleep(3) # NEEDS TO BE REFACTORED
+      expect(page).to have_content("TEST THIS")
     end
   end
 
--- spec/helpers/sessions_helper.rb
+++
@@ -1,5 +0,0 @@
-module SessionsHelper
-  def current_user
-    @user = User.find session[:user_id] if session[:user_id]
-  end
-end
