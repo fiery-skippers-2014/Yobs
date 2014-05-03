65bb2eea2d9378382203c9b88528f3dbfbc27093
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-03 14:08:51 -0700

    Added logout functionality   * update routes to reach destroy   * cleaned up Sessions Controller   * fixed link in html form
    TODO: Finish Feature Tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,16 @@ YOBS::Application.routes.draw do
 
   resources :categories, :only => [:index, :show]
   resources :users
+  delete 'session' => 'sessions#destroy'
   resources :sessions, :only => [:new, :create, :destroy]
   root :to => 'categories#index'
 
-  get "log_out" => "sessions#destroy", :as => "log_out"
+  # get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
 
-  match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  # match '/signin',  to: 'sessions#new',         via: 'get'
+  # match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 
 end
