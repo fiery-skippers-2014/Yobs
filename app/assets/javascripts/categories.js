e49d747ce6eddb33f4b988c3e8a6c96c2042213a
laura <llcossette@gmail.com>
2014-05-03 08:55:55 -0700

    * remove unecessary ajax event for jobs read more, replaced with simple js event * removed unused jobs route from controller and route file

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -1,20 +1,13 @@
 var Job = {
 	init: function() {
-		$('.more-btn').on('ajax:success', this.expandDescription);
-		$('.more-btn').on('ajax:error', this.appendError);
+		$('.more-btn').on('click', this.expandDescription);
 	},
 
-	expandDescription: function(event, data) {
-		event.preventDefault();
+	expandDescription: function() {
 		console.log("success");
-		console.log(data);
 		$($(this).parents('.job-box').children('.job-desc').children('.long-desc')).toggle();
-	},
-
-	appendError: function(event, data) {
-		event.preventDefault();
-		console.log(data);
 	}
+
 }
 
 $(document).ready(function() {
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,3 @@
 class JobsController < ApplicationController
-  def show
-  	@job = Job.find(params[:id])
-  	render :partial => 'jobs/extended_display', :locals => { :job => @job }
-  end
+
 end
