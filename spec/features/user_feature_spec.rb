7e01e66a13e33de974d1557949791a8982217066
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-03 14:24:00 -0700

    adds log in test on user feature spec

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 
  describe 'Guest users' do
-   context 'can create an account on the site' do
-     let(:user) {FactoryGirl.create :user}
+  context 'can create an account on the site' do
+    let(:user) {FactoryGirl.create :user}
     it "can fill out the Create a User form and create an account" do
         visit root_path
         fill_in 'user[user_name]', :with => user.user_name
@@ -13,9 +13,20 @@ require 'spec_helper'
         fill_in 'user[password]', :with => user.password
         click_on "Create User"
         expect(page).to have_content user.user_name
-      end
-   end
-
+    end
   end
+
+  context 'can login with valid email and password' do
+    let!(:user) {FactoryGirl.create :user}
+    it "can fill out the Create a User form and create an account" do
+        visit root_path
+        fill_in 'email', :with => user.email
+        fill_in 'password', :with => user.password
+        click_on "Log in"
+        expect(page).to have_content user.user_name
+    end
+  end
+
+end
 
 
