969123e4b85acfee87d6d35e1eb44d5918ce018b
David Wilbanks & Hembaba <pair+dwilbank68.hembaba@devbootcamp.com>
2014-05-05 15:52:59 -0700

    added tests for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 describe UsersController do
+
+  let(:user) { FactoryGirl.create :user}
+  before(:each) do
+    ApplicationController.any_instance.stub(:current_user).and_return(user)
+  end
+
   context "#create" do
     it "should create a user with valid params" do
       expect {
         post :create, :user => FactoryGirl.attributes_for(:user)
         }.to change {User.count}.by(1)
+      end
     end
+
+    context "show" do
+      it "is successful" do
+        get :show, :id => user.id
+        expect(response).to be_success
+      end
+
+      it "assigns @user to User found by id" do
+        get :show, :id => user.id
+        expect(assigns(:user)).to eq user
+      end
+    end
+
+
   end
-end
 
