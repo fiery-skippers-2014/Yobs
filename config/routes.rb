90f2498cc6c405c3f8a5ba5a1f7c42f1ec3679e1
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-04 21:41:15 -0700

    Added the delete route and the interest resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,13 @@ YOBS::Application.routes.draw do
   delete 'session' => 'sessions#destroy'
   resources :sessions, :only => [:new, :create, :destroy]
 
-  resources :interests, :only => [:create]
+  delete 'interests' => 'interests#destroy'
+  resources :interests, :only => [:create, :destroy]
   root :to => 'categories#index'
 
   resources :agencies, :only => [:show] do
     resources :jobs
   end
-
 
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
