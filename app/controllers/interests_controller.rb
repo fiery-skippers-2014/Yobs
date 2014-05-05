710dba89b7d112606374f93a1d34da15123cea0d
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-04 21:40:30 -0700

    Added Create and Destroy methods to Interest Controller
      *cleaned out first version without AJAX

--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -1,15 +1,21 @@
 class InterestsController < ApplicationController
 
+skip_before_filter  :verify_authenticity_token
+
   def create
-  	puts params
-	interest = Interest.new(:job_id => params["job_id"], :user_id => params["user_id"])
-	puts interest.job_id
-	if interest.save
-		redirect_to :back
-	else
-		puts interest.errors.full_messages.join(' : ')
-		redirect_to :back
-	end
+    interest = Interest.new(:job_id => params["job_id"], :user_id => current_user.id)
+  	if interest.save
+      render json: "#job-#{interest.job_id}".to_json
+  	else
+  		render json: interest.errors.full_messages.join(' : ')
+  	end
+  end
+
+  def destroy
+    interest = current_user.interests.find_by_job_id(params["job_id"])
+    job_id = interest.job_id
+    interest.destroy
+    render json: "#job-#{job_id}".to_json
   end
 
 end
