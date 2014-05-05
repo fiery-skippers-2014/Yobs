19c7a6d4f61a1d2fe6c31d85c17ab34538c75096
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-04 21:34:41 -0700

    Added two job functions to save and unsave interests

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -17,21 +17,45 @@ var Job = {
 			footer_tag = 'Read more...'
 			console.log('more')
 		}
-
-
-		//add in logic to check if long-desc is showing. if it is, change 'Read more' to 'less'
-
-		// $($(this).parents('.job-box').children('.job-desc').children('.long-desc')).toggle();
-		$($(this).closest('.job-box').find('.long-desc')).toggle();
+			// add in logic to check if long-desc is showing. if it is, change 'Read more' to 'less'
+			$($(this).closest('.job-box').find('.long-desc')).toggle();
 			return false; // prevent default click action from happening!
 	     	e.preventDefault(); // same thing as above
+	},
+
+	saveInterest: function(event) {
+		event.preventDefault()
+		job_id = event.target.classList[1].substring(4)
+		interest_data = {job_id: job_id}
+		$.ajax({
+			url: '/interests',
+			type: 'POST',
+			data: interest_data
+		}).success(function(data){
+			console.log(data)
+				$(data).css('background-color','#DDD')
+				$(data).find('.interest-btn').hide()
+				$(data).find('.notinterest-btn').show()
+		}
+		).error(function (){console.log("ERROR")})
+	},
+
+	noInterest: function(event) {
+		event.preventDefault()
+		job_id = event.target.classList[1].substring(4)
+		interest_data = {job_id: job_id}
+		$.ajax({
+			url: '/interests',
+			type: 'DELETE',
+			data: interest_data
+		}).success(function(data){
+				console.log(data)
+				$(data).css('background-color','#FFF')
+				$(data).find('.interest-btn').show()
+				$(data).find('.notinterest-btn').hide()
+			}
+		).error(function (){console.log("ERROR")})
+
 	}
 
 }
-
-// $(document).ready(function() {
-// 	// Job.init();
-// 	// $('.long-desc').hide();
-// })
-
-
