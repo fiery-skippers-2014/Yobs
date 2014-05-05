39230fd80d775e47cdbe9bb97a6502a46d7eba2d
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-04 21:39:32 -0700

    Added a before filter current user to make sure all controllers and indexes get current user access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # include Authentication
   protect_from_forgery
   helper_method :current_user
-
+  before_filter :current_user
   def agency?(user)
     return true if user.role == "agency"
   end
