4995dc0441a566baab8ad453fd5753178000627f
Jonathan Wrobel & laura <pair+jwrobes.llcossette@devbootcamp.com>
2014-05-04 21:37:48 -0700

    Cleaned out commented out code   * removed sample form from form   * removed uneeded Job.init   * added job init to Categories show html

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -39,34 +39,10 @@ var NewJobFormDropDown = function () {
 
 $(document).ready(function(){
 	UserStuff.init();
-	Job.init();
 	$('.long-desc').hide();
-
 	$(function() {
 		$( "#datepicker" ).datepicker();
 	});
 
 })
-
-
-// <<div class="header">
-
-//    TEST THIS
-//   <% if !current_user %>
-//     <a href="#" id="login-link">Login</a>
-//     <a href="#" id="new-user-link">Register</a>
-//   <%else%>
-//     <%= current_user.user_name %>
-//     <%= link_to "Log Out", session_path, method: :delete %>
-//   <% end %>
-
-
-//   <div class="login-form">
-//     <%= form_tag '/sessions' %>
-//       <p> <%= email_field_tag :email, nil, :placeholder => "Email" %> </p>
-//       <p> <%= password_field_tag :password, nil, :placeholder => "Password" %> </p>
-//       <p> <%= submit_tag "Log in", id: 'login-submit-btn' %> </p>
-//   </div>
-
-// </div>
 
