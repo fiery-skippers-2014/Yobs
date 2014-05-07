7eece9f3f59bf9d48bccfca7a779fd9d5143b275
laura <llcossette@gmail.com>
2014-05-07 16:14:45 -0700

    agency users don not have job request info on their profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
 
-  # def job_created_at(user, job)
-  #   Interest.where('user_id = ? and job_id = ?', user.id, job.id).first.created_at
-
-  # end
+  def agency?(user)
+    user.role == 'agency'
+  end
 
 end
