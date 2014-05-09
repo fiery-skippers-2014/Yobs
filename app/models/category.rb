2ab3674c2f04b52ed98b8f8edb53d62c55a435d4
Jonathan Wrobel <jwrobes@gmail.com>
2014-05-08 20:16:52 -0700

    changed validation for categories to fit real category names

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,6 @@ class Category < ActiveRecord::Base
   has_many :jobs
 
   validates :name,	:presence => true,
-     				:length => {:minimum => 3, :maximum => 12}
+     				:length => {:minimum => 3, :maximum => 24}
 
 end
